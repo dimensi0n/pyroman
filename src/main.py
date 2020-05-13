26d27eeb95f987c597b04b79ea072612c9be395d
Erwan <e-roussel@outlook.com>
2020-05-13 11:48:26 +0200

    Remove unused factors var

--- src/main.py
+++ src/main.py
@@ -3,8 +3,6 @@ def toRoman(number):
     romans = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     result = ''
 
-    factors = []
-
     for letter in romans.keys():
         while romans[letter] <= number:
             result += letter
