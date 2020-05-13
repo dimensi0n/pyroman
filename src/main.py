59510d380e3e0dc239aa1282efff997232d3409d
Erwan ROUSSEL <e-roussel@outlook.com>
2020-05-13 14:58:25 +0200

    Add numbers to list

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 def toRoman(number):
     number = number
-    romans = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    romans = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9,'V': 5, 'IV': 4, 'I': 1}
     result = ''
 
     for letter in romans.keys():
